5c6eb2118fa30d34a6d8ffefa4e098dc7a78b57d
Junsup Lee <jlee651@illinois.edu>
2019-12-28 16:52:30 -0600

    yoyo

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to EPL Chant Generator of season 2019-2020")
+print("here is the instructions")
+print("1. first enter the name of the team you would like to view the chant of \n")
+print("2. 
