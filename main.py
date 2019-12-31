ed66fd112b4a58e763dd155aeae103f7db54eb65
Junsup Lee <jlee651@illinois.edu>
2019-12-31 16:24:25 -0600

    push changes

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import os
+
+print("----------------------------------------------------------------")
+print("Program Written by Junsup Lee naya6408 on github")
 print("Welcome to EPL Chant Generator of season 2019-2020")
-print("here is the instructions")
-print("1. first enter the name of the team you would like to view the chant of \n")
-print("2. 
+print("Here are the instructions")
+print("This program allows users to generate epl chants of any given team of 2019-2020 season")
+print("Here are the list of teams that are available")
+
+
+newdir = os.chdir("/Users/Junsup/Desktop/eplsongsGenerator/Teams")
+teamList =os.listdir()
+
+
+teamName = input("Please enter the name of the team you would like to view the chant of:  \n")
+
+if(teamName in teamList):
+    print("Correct Input. Thank you for the input")
+else:
+    print("Wrong input")
