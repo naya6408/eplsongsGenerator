956622cf219ec110daabcb7dbb45610407ee5711
Junsup Lee <junsuplee@Junsups-MacBook-Pro.local>
2020-01-11 14:10:00 -0600

    add

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import os
 import pprint
 
 def setteamDirectory(teamName):
-    newDir = "/Users/Junsup/Desktop/eplsongsGenerator/Teams" + "/" + teamName
+    newDir = "/Users/junsuplee/Desktop/eplsongsGenerator/Teams" + "/" + teamName
     return newDir
 
 
-print("----------------------------------------------------------------")
-print("Program Written by Junsup Lee naya6408 on github")
+print("----------------------------------------------------")
+print("-Program Written by Junsup Lee (naya6408) on github-")
+print("----------------------------------------------------")
 print("Welcome to EPL Chant Generator of season 2019-2020")
-print("Here are the instructions")
 print("This program allows users to generate epl chants of any given team of 2019-2020 season")
 print("Here are the list of teams that are available")
 
 
-newdir = os.chdir("/Users/Junsup/Desktop/eplsongsGenerator/Teams")
-teamList =os.listdir()
+currentDirectory = os.getcwd()
+newDirectory = currentDirectory + "/Teams"
+teamList =os.listdir(newDirectory)
 tempTeamlist = [x.lower() for x in teamList]
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(tempTeamlist)
-
 
 
 teamName = input("Please enter the name of the team you would like to view the chant of:  \n")
