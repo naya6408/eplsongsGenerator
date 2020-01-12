e3690d645fb95b2fa6d10805bade9786b718ff18
Junsup Lee <junsuplee@Junsups-MacBook-Pro.local>
2020-01-11 23:52:21 -0600

    changes

--- main.py
+++ main.py
@@ -22,51 +22,45 @@ def outputSong(filename, chantName):
     f = open(selectedChant, "r")
     print(f.read())
 
+def main():
+    print("----------------------------------------------------")
+    print("-Program Written by Junsup Lee (naya6408) on github-")
+    print("----------------------------------------------------")
+    print("Welcome to EPL Chant Generator of season 2019-2020")
+    print("This program allows users to generate epl chants of any given team of 2019-2020 season")
+    print("Here are the list of teams that are available")
 
-print("----------------------------------------------------")
-print("-Program Written by Junsup Lee (naya6408) on github-")
-print("----------------------------------------------------")
-print("Welcome to EPL Chant Generator of season 2019-2020")
-print("This program allows users to generate epl chants of any given team of 2019-2020 season")
-print("Here are the list of teams that are available")
+    currentDirectory = os.getcwd()
+    newDirectory = currentDirectory + "/Teams"
+    teamList =os.listdir(newDirectory)
+    tempTeamlist = [x.lower() for x in teamList] #putting all the team into lower cases for consistency
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(tempTeamlist)
+
+    while True:
+        teamName = str(input("Please enter the name of the team you would like to view the chant of: "))
+        teamName = teamName.lower().strip(" ")
+        if(teamName in tempTeamlist):
+            print("Correct Input. Thank you for the input")
+            ourteamDir = setteamDirectory(teamName)
+            break
+        elif(teamName == "exit"):
+            print("exiting program")
+            sys.exit()
+        else:
+            print(teamName, " is a wrong input. Please check your input")
+            print("please refer to the list of teams above.")
+            print("press type exit if you would like to exit program")
+            continue
+    teamDirectory = setteamDirectory(teamName)
+    os.chdir(teamDirectory)
+    print("here are the available list of chants for " + teamName)
+    songList = os.listdir(teamDirectory)
+    songList = createsongList(songList)
+    print("the list of songs are: ", songList)
+    selectedChant = str(input("Please select your song from the list: "))
+    outputSong(selectedChant, selectedChant)
 
 
-currentDirectory = os.getcwd()
-newDirectory = currentDirectory + "/Teams"
-teamList =os.listdir(newDirectory)
-
-tempTeamlist = [x.lower() for x in teamList] #putting all the team into lower cases for consistency
-
-
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(tempTeamlist)
-
-while True:
-    teamName = str(input("Please enter the name of the team you would like to view the chant of: "))
-    teamName = teamName.lower().strip(" ")
-    if(teamName in tempTeamlist):
-        print("Correct Input. Thank you for the input")
-        ourteamDir = setteamDirectory(teamName)
-        break
-    elif(teamName == "exit"):
-        print("exiting program")
-        sys.exit()
-    else:
-        print(teamName, " is a wrong input. Please check your input")
-        print("please refer to the list of teams above.")
-        print("press type exit if you would like to exit program")
-        continue
-        
-    
-
-teamDirectory = setteamDirectory(teamName)
-os.chdir(teamDirectory)
-print("here are the available list of chants for " + teamName)
-songList = os.listdir(teamDirectory)
-songList = createsongList(songList)
-print("the list of songs are: ", songList)
-
-selectedChant = str(input("Please select your song from the list: "))
-
-outputSong(selectedChant, selectedChant)
-
+if __name__ == "main":
+    main()
